94386fe00a29e17ec28c326c3dd752af7b13d284
Oscar Calles <ocalles@gmail.com>
2013-12-12 11:28:22 +0100

    Cocoa Programming L37 - NSError
    Cocoa Programming L37 - NSError

--- CocoaExamples6/AppDelegate.m
+++ CocoaExamples6/AppDelegate.m
@@ -9,10 +9,25 @@
 #import "AppDelegate.h"
 
 @implementation AppDelegate
+@synthesize window =_window;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+}
+
+-(IBAction)saveSomeText:(id)sender
+{
+    NSString *someText =@">>>>>> This text is going to be saved and we will look the error that is returned <<<<<<<";
+    
+    NSError *error = nil;
+    BOOL result = [someText writeToFile:@"/myText.txt" atomically:YES encoding:NSUTF8StringEncoding error:&error];
+    if(result == NO){
+        NSLog(@"%@", [error domain]);
+        NSLog(@"%ld",[error code]);
+        NSLog(@"%@",[error localizedDescription]);
+        [[NSAlert alertWithError:error] runModal];
+    }
 }
 
 @end
